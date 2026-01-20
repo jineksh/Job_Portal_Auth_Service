ce5055cbe961902076b106b33a6871142c42d11c
jineksh <chovatiyajinex@gmail.com>
2026-01-20 21:25:17 +0530

    testing reverse-proxy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import {PORT} from './config/server.js';
+import { PORT } from './config/server.js';
 import ApiRoutes from './routes/index.js';
 import { errorMiddleware } from "./middleware/errorHandler.js";
 import emailWorker from './worker/email.js';
@@ -12,21 +11,25 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000', // Specify your frontend URL
-    credentials: true,               // Allow cookies/headers
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ limit: '20mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use('/api',ApiRoutes);
+app.get('/home', (req, res) => {
+    res.send('Auth Service is running');
+});
+
+app.use('/api', ApiRoutes);
+
 emailWorker('emailQueue');
 
 app.use(errorMiddleware);
 
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Auth Service is running on port ${PORT}`);
-})
+});
